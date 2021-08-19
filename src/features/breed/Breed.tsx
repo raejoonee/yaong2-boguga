d5a042fc74a2bedc53705bae6c1b9ebbbb2e30f6
raejoonee <crj0901@gmail.com>
2021-08-19 17:43:40 +0900

    fix : 검색 시 대소문자 구분 없이 검색할 수 있도록 수정한다.

--- src/features/breed/Breed.tsx
+++ src/features/breed/Breed.tsx
@@ -3,10 +3,18 @@ import { RootState } from '../../store';
 
 const Breed = () => {
   const breeds = useSelector((state: RootState) => state.breed.breeds);
+  const convertUppercase = (str: string) => {
+    const splited = str.toLowerCase().split(' ');
+    const converted: string[] = [];
+    splited.forEach((e) => {
+      converted.push(e.charAt(0).toUpperCase() + e.substring(1));
+    });
+    return converted.join(' ');
+  };
   return (
     <datalist id="cat-list">
       {breeds.map((breed) => (
-        <option key={breed} value={breed} />
+        <option key={breed} value={convertUppercase(breed)} />
       ))}
     </datalist>
   );
